ae3c37ca830da54e1caa28218fbdb1c666779f67
jy-hwang <stratojy@gmail.com>
2025-04-18 13:22:26 +0900

    feat. Mapper 객체 생성하기
    1. mybatis 라이브러리 추가
    2. Dao 객체를 생성하고 @Mapper 어노테이션 붙이기
    3. ServiceImpl 객체에 @Service 어노테이션 붙이기
    4. mariadb 연결정보 작성

--- src/main/java/com/newlecture/web/dao/NoticeDao.java
+++ src/main/java/com/newlecture/web/dao/NoticeDao.java
@@ -1,10 +1,14 @@
 package com.newlecture.web.dao;
 
 import java.util.List;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 import com.newlecture.web.entity.Notice;
 
+@Mapper
 public interface NoticeDao {
 
+  @Select("SELECT * FROM notice")
   List<Notice> getList();
 
   Notice getOne(int id);
--- src/main/java/com/newlecture/web/service/NoticeServiceImpl.java
+++ src/main/java/com/newlecture/web/service/NoticeServiceImpl.java
@@ -2,9 +2,11 @@ package com.newlecture.web.service;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import com.newlecture.web.dao.NoticeDao;
 import com.newlecture.web.entity.Notice;
 
+@Service
 public class NoticeServiceImpl implements NoticeService {
 
   @Autowired
